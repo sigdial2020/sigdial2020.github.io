837513e9d2d232dc3543a58559ffd21e8999b63f
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2020-06-18 23:03:23 +0200

    timeline table version

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
         }),
         $.ajax({
            type: "GET",
-           url: "/content/grouping2.csv",
+           url: "/content/grouping.csv",
            //dataType: "text",
            success: function(data2){
                 return data2;
@@ -43,29 +43,61 @@ $(document).ready(function() {
         $(table_data).each(function (i, rowData) {
             if (rowData[2]=="Break"){
             return;}
-            var row = $('<tr class="col-md-9 col-sm-12" id=schedule_'+i+'></tr>');
+            var row = $('<div class="row" id=schedule_'+i+'></div>');
             dates=get_dates(rowData[1],rowData[0][2])
 
+            date_row=$("<div class='row'></div>")
             for (index = 0; index < dates.length; index++){
-                row.append("<td>"+dates[index]+"</td>")
+                date_row.append("<div style='width:14%' class='col-md-1'>"+dates[index]+"</div>")
             }
+            date_col=$("<div class='col-md-4' align='center'></div>")
+            date_col.append(date_row)
 
-            div_=$('<td></td>')
+            if (rowData[7] != ""){
+                dates=get_dates(rowData[7],rowData[0][2])
+                date_row=$("<div class='row'></div>")
+                for (index = 0; index < dates.length; index++){
+                    date_row.append("<div style='width:14%' class='col-md-1'>"+dates[index]+"</div>")
+                }
+                date_col.append(date_row)
+            }
+            row.append(date_col)
 
-            div=$('<div class="member stephen"></div>')
+
+
+            div_=$('<div class="col-md-8 "></div>')
+            div=$('<div class=" member stephen"></div>')
             title='<a name="oral1" href="#" class="btn-large bg-2"><img src="assets/img/microphone.png" alt="">'+rowData[2]+'</a>'
 
             div.append($('<div class="button">'+title+'</div><h4>'+rowData[3]+'</h4></div>'))
-            papers_ids=rowData[4].split(", ")
+            papers_ids=rowData[4].split(";")
 
-            for (idx in papers_ids){
-                paper_id=papers_ids[idx]
-                if (paper_id in papers){
-                    div.append($("<p><bold>"+papers[paper_id][2]+papers[paper_id][1]+" <br></p>"))
-            }}
+            if (rowData[2]== "Keynote"){
+                div.append($("<br><a href="+rowData[8]+">Abstract</a>"))
+            }
+            if (papers_ids != ""){
+                if (["Prerecord stream","Session"].includes(rowData[2])){
+                    console.log(rowData[2])
+
+                    div.append($("<h5>Prerecord stream:</h5> "))
+                }
+                for (idx in papers_ids){
+                    paper_id=papers_ids[idx]
+                    if (paper_id in papers){
+                        div.append($("<p>"+papers[paper_id][2]+"<br>"+papers[paper_id][1]+" </p>"))
+                    }
+                }
+            }
+
+            if (rowData[6]!= ""){
+                div.append($("<br><h5>"+rowData[6]+"</h5>"))
+            }
 
             div_.append(div)
             row.append(div_)
+
+
+
 
             day_key="tab"+rowData[0][2]
             //tables[day_key].append(row);
@@ -77,6 +109,7 @@ $(document).ready(function() {
     }
 
     function get_dates(date, day){
+        console.log(date)
         countries=[-7,-6,-4,2,5.50,8,9]
         date_split=date.split(":")
         times=[]
@@ -84,7 +117,7 @@ $(document).ready(function() {
 
         for (index = 0; index < countries.length; index++){
             date_=new Date(d.getTime())
-            date_.setHours(date_.getHours()+countries[index])
+            date_.setHours(date_.getHours()-countries[index])
             //local_time=new Date(2020,7,day,date_split[0],date_split[1]+countries[index],0,0)
             //var time = d.toLocaleString("en-GB", {timeZone: "Japan"});
             var time = date_.toLocaleString("en-GB");
