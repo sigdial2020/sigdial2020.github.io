e3e014a881646e07a81bc8b721d7866eb3f6c3dc
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2020-06-22 21:31:07 +0200

    table version of schedule

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -1,7 +1,3 @@
-Date.prototype.addHours = function(h) {
-  this.setTime(this.getTime() + (h*60*60*1000));
-  return this;
-}
 $(document).ready(function() {
 
     $.when(
@@ -37,28 +33,32 @@ $(document).ready(function() {
             day_key="tab"+rowData[0][2]
             idx_color=get_color_idx(rowData[2])
             schedule_timeline=$("#schedule_timeline_"+rowData[0][2])
+            row_event=$("<tr></tr>")
 
-            append_date(rowData[1],rowData[0][2], schedule_timeline, rowData[3])
-            if (rowData[7] != ""){
-                append_date(rowData[7],rowData[0][2],schedule_timeline,rowData[6])
-            }
-            if (rowData[2]=="Break"){
-            return;}
-            var row = $('<div class="row" id=schedule_'+i+'></div>');
+            append_date(rowData[1],rowData[0][2], row_event, rowData[3])
 
-            div_=$('<div class="col-md-12 "></div>')
-            div=$('<div class="member stephen"></div>')
-            title='<a href="#" class="btn-large bg-'+idx_color+'"><img src="assets/img/microphone.png" name='+id_row+'>'+rowData[2]+'</a>'
+            //if (rowData[2]=="Break"){
+            //    return;}
+            //var row = $('<div class="row" id=schedule_'+i+'></div>');
 
-            div.append($('<div class="button color">'+title+'</div><h4>'+rowData[3]+'</h4></div>'))
-            papers_ids=rowData[4].split(";")
+            div_=$('<td></td>')
+            div=$('<div class="member stephen" style="background:none;margin-bottom:0px" ></div>')
+
+            microphone=''
+            if (rowData[2]!= "Break"){
+                microphone='<img src="assets/img/microphone.png">'
+                }
+            title='<a href="#" class="btn-large bg-'+idx_color+'">'+microphone+rowData[2]+'</a>'
+
+            div.append($('<div class="button color" style="margin-top:-5%">'+title+'</div><h4>'+rowData[3]+'</h4></div>'))
+            papers_ids=rowData[4].split(",")
 
             if (rowData[2]== "Keynote"){
-                div.append($("<br><a href="+rowData[8]+">Abstract</a>"))
+                div.append($("<br><a href="+rowData[6]+">Abstract</a>"))
             }
-            if (papers_ids != ""){
+            if (papers_ids != "" && rowData[2]=="Keynote"){
                 if (["Prerecord stream","Session"].includes(rowData[2])){
-                    div.append($("<h5>Prerecord stream:</h5> "))
+                    div.append($("<h5>Prerecord stream:</h5>"))
                 }
                 for (idx in papers_ids){
                     paper_id=papers_ids[idx]
@@ -67,12 +67,11 @@ $(document).ready(function() {
                     }
                 }
             }
-            if (rowData[6]!= ""){
-                div.append($("<br><h5>"+rowData[6]+"</h5>"))
-            }
             div_.append(div)
-            row.append(div_)
-            $("#"+day_key).append(row)
+            //row.append(div_)
+            row_event.append(div_)
+            schedule_timeline.append(row_event)
+            //$("#"+day_key).append(row)
         });
     }
 
@@ -80,31 +79,38 @@ $(document).ready(function() {
         countries=[-7,-6,-4,2,5.5,8,9]
         date_split=date.split(":")
         times=[]
-        var d = new Date(Date.UTC(20, 7,parseInt(day),  parseInt(date_split[0])+7, date_split[1], 0, 0));
+
+        var d = new Date(Date.UTC(20, 7,parseInt(day), parseInt(date_split[0])+7, date_split[1], 0, 0));
         for (index = 0; index < countries.length; index++){
             date_=new Date(d.getTime()+ parseFloat(countries[index])*60*60*1000)
             var time = date_.toUTCString();
+            //console.log(date_)
+            //console.log(time)
             times.push(time.split(" ")[4].slice(0,5))
         }
         return times
     }
 
     function get_color_idx(type_key){
-        if (["Keynote","Talk","Demo"].includes(type_key)){
-            return 1
+        if (["Live QA"].includes(type_key)){
+            return 6
         }
         else {
             return 2
             }
     }
 
-    function append_date(time_reference, day, timeline, title_schedule){
+    function append_date(time_reference, day, row, title_schedule){
         dates=get_dates(time_reference ,day)
-        dates_str=[]
         for (index = 0; index < dates.length; index++){
-            dates_str.push(dates[index])
+            row.append($("<td style='vertical-align:middle'><h4>"+dates[index]+"</h4></td>"))
+
+            //dates_str.push(dates[index])
         }
-        dates_str=dates_str.join(" ")
-        timeline.append($("<li class='color-"+idx_color+"'><a style='width:100%'  href=#"+id_row+">"+dates_str+"</a><br>"+title_schedule+"</li>"))
+
+        //dates_str=dates_str.join(" ")
+        //timeline.append($("<li class='color-"+idx_color+"'><a style='width:100%'  href=#"+id_row+">"+dates_str+"</a><br>"+title_schedule+"</li>"))
+        //return title_schedule
+        //return $("<td class='color-"+idx_color+"'><div class='col-md-5'><a style='width:100%'  href=#"+id_row+">"+dates_str+"</a></div><br></li>")
     }
    });
